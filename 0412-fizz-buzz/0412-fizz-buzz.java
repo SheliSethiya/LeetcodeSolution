9232439db2fd7c94571d1d6f5c3e3bcf8ea493e3
Sheli Sethiya <shelisethiya@gmail.com>
2026-09-17 23:17:21 +0530

    Time: 1 ms (99.84%), Space: 46.8 MB (65.74%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> li=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0){
+                li.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                li.add("Fizz");
+            }
+            else if(i%5==0){
+                li.add("Buzz");
+            }
+            else{
+                li.add(String.valueOf(i));
+            }
+        }
+
+        return li;
+    }
+}
