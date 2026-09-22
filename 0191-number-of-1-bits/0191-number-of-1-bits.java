b0a77f68d149b1e328cf35bcb3138901bf126272
Sheli Sethiya <shelisethiya@gmail.com>
2026-09-22 23:15:30 +0530

    Time: 0 ms (100%), Space: 42.6 MB (28.13%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,17 +1,29 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count = 0;
+        // int count = 0;
 
-        while (n > 0) {
-            int rem = n % 2;
+        // while (n > 0) {
+        //     int rem = n % 2;
 
-            if (rem == 1) {
+        //     if (rem == 1) {
+        //         count++;
+        //     }
+
+        //     n /= 2;
+        // }
+
+        // return count;
+
+       
+        int count=0;
+        for(int i=0;i<32;i++){
+            if((n&1)==1){
                 count++;
             }
-
-            n /= 2;
+            n>>=1;
         }
-
         return count;
+    
+
     }
 }
